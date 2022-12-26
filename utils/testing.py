ba473c7a2dea857212a797e8f0339d47b1ba852c
Eduard Romaniuk <eduard.romanyuk1990@gmail.com>
2022-12-26 11:46:23 +0100

    add arguments to failed test log message

--- utils/testing.py
+++ utils/testing.py
@@ -1,4 +1,4 @@
 def test(name, func, args, expected):
     result = func(*args)
-    assert result == expected, f'Expected: {expected}, Actual: {result}'
+    assert result == expected, f'\nExpected: {expected}, Actual: {result}\nArguments: {args}'
     print(f"Test case '{name}' passed")
